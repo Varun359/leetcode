81fc4796013d89dc7152c3b4d31ca9ef2745b496
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-06 21:33:30 -0700

    Time: 90 ms (33.03%), Space: 54.2 MB (49.48%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,21 +1,4 @@
 class Solution {
-    Boolean dp[][];
-    public boolean recursiveHelper(int nums[], int l, int sum)
-    {
-        if(sum==0)
-            return true;
-        if(l==0 && sum!=0)
-            return false;
-        
-        if(dp[l][sum] != null)
-            return dp[l][sum];
-        
-        if(nums[l-1] <= sum)
-         return dp[l][sum] = recursiveHelper(nums,l-1,sum-nums[l-1]) || recursiveHelper(nums, (l-1), (sum));
-        else
-         return dp[l][sum] = recursiveHelper(nums,l-1,sum);
-    }
-    
     public boolean canPartition(int[] nums) {
         int i,j,sum=0;
         for(i=0;i<nums.length;i++)
@@ -23,14 +6,21 @@ class Solution {
         if(sum%2==1)
             return false;
         sum/=2;
-        dp= new Boolean[nums.length+1][sum+1];
+        boolean dp[][] = new boolean[nums.length+1][sum+1];
+        
         for(i=1;i<sum+1;i++)
          dp[0][i] = false;
         for(j=0;j<nums.length+1;j++)
          dp[j][0] = true;
         
-      
-         return recursiveHelper(nums, nums.length, sum);
-       
+        for(i=1;i<nums.length+1;i++)
+         for(j=1;j<sum+1;j++)
+         {
+             if(nums[i-1]<=j)
+                 dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+             else
+                 dp[i][j] = dp[i-1][j];  
+         }
+        return dp[nums.length][sum];
     }
 }
