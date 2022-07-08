73c4ff56e32024996461db1d5b3d9a9fad42d522
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-07 23:52:31 -0700

    Time: 2 ms (87.37%), Space: 57.9 MB (45.94%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -3,27 +3,18 @@ class Solution {
         
         if(nums==null || nums.length ==0 )
             return new int[0];
-        int prefix_prod[] = new int[nums.length];
-        int suffix_prod[] = new int[nums.length];
-        
+        int prod[] = new int[nums.length];
         int i;
-        prefix_prod[0] = 1;
+        prod[0] = 1;
         for(i=1;i<nums.length;i++)
-        {
-            prefix_prod[i] = prefix_prod[i-1]*nums[i-1];
-        }
-    
-        suffix_prod[nums.length-1] = 1;
-        for(i=nums.length-2;i>=0;i--)
-        {
-            suffix_prod[i] = suffix_prod[i+1] * nums[i+1];
-        }
-    
-        for(i=0;i<nums.length;i++)
-        {
-         nums[i] = prefix_prod[i] * suffix_prod[i];   
-        }
+            prod[i] = prod[i-1]*nums[i-1];
         
-        return nums;
+        int temp = 1;
+        for(i=nums.length-1;i>=0;i--)
+        {
+            prod[i] = prod[i]*temp;
+            temp*= nums[i];
+        }
+        return prod;
     }
 }
