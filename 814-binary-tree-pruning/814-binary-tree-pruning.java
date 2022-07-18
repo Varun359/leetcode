49fa8ebe2a986da5a3945c4ac0d25359b0da3c06
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-17 20:39:23 -0700

    Time: 0 ms (100.00%), Space: 42.1 MB (19.35%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.java
+++ 814-binary-tree-pruning/814-binary-tree-pruning.java
@@ -14,21 +14,14 @@
  * }
  */
 class Solution {
-    public boolean hasOne(TreeNode root)
-    {
-        if(root.val == 0)
-            return false;
-        return true;
-    }
     public TreeNode pruneTree(TreeNode root) {
        if(root == null)
            return null;
        root.left = pruneTree(root.left);
        root.right = pruneTree(root.right);
         
-       
-       if(!hasOne(root) && root.left == null && root.right == null)
-         root = null;
-       return root;
+       if(root.val == 0 && root.left == null && root.right == null)
+         return null;
+        return root;
     }
 }
