b063f29053cee862f71a53bfad5276ba40f0abf6
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-18 18:52:17 -0700

    Time: 2 ms (99.75%), Space: 122 MB (81.33%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -5,21 +5,27 @@ class Solution {
         if(nums == null && nums.length == 0)
             return 0;
             
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        
-        for(int i=0;i<nums.length ; i++)
-          pq.add(nums[i]);
-        
-        int i = 0, num =1;
+        int i = 0;
         while(i<nums.length)
         {
-            if(pq.poll() == num)
-            {
-                num++;
-            }
-            i++;
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[nums[i] - 1])
+               swap(nums, i , nums[i]-1);
+            else
+               i++;
         }
-        return num;
         
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i] != i+1)
+                break;
+        }
+        return i+1;
+    }
+    
+    public void swap(int arr[], int i, int j)
+    {
+        int t = arr[i];
+        arr[i] = arr[j];
+        arr[j] = t;
     }
 }
