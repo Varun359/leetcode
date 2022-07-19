ad33d36c5a6f1fba25b7fd386436e60df60e0ef0
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-19 14:33:11 -0700

    Time: 7 ms (65.99%), Space: 43.8 MB (52.31%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.java
@@ -2,21 +2,26 @@ class Solution {
 
     public int numOfPairs(String[] nums, String target) {
        
-        int count = 0;
-        int len = nums.length;
-        for(int i = 0;i < len;i++)
-        {
-            for(int j = i+1; j < len; j++)
-            {
-                String s = nums[i]+nums[j];
-                if(s.compareTo(target) == 0)
-                    count++;
-                String t = nums[j]+nums[i];
-                if(t.compareTo(target) == 0)
-                    count++;
-            }
-        }
-        
-        return count;
+      Map<String, Integer> hm = new HashMap();
+     
+      for(int i=0;i<nums.length;i++)
+          hm.put(nums[i], hm.getOrDefault(nums[i],0)+1);
+      
+      int res = 0;
+      for(int i=1;i<target.length();i++)
+      {
+          String s = target.substring(0,i);
+          String t = target.substring(i,target.length());
+          
+          if(hm.containsKey(s) && hm.containsKey(t))
+          {
+              if(s.equals(t))
+                res += hm.get(s) * (hm.get(s)-1);
+              else
+                res += hm.get(s) * hm.get(t);
+          }
+          
+      }
+        return res;
     }
 }
