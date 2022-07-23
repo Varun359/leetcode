f18b9eed2395e021f66358b0e932842ec386dae3
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-23 13:18:08 -0700

    Time: 1 ms (99.04%), Space: 44.3 MB (49.70%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -19,8 +19,10 @@ class Solution {
         if(root==null)
             return 0;
         int lh = height(root.left);
+        if(lh == -1)
+            return -1;
         int rh = height(root.right);
-        if(lh == -1 || rh == -1)
+        if(rh == -1)
             return -1;
         if(Math.abs(lh-rh)>1)
             return -1;
