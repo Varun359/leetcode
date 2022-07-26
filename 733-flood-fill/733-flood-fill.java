7b04251c41cbe2420de19ecaae9632dcf2e76d6a
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-07-25 19:13:39 -0700

    Time: 1 ms (90.40%), Space: 48 MB (48.66%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -4,40 +4,22 @@ class Solution {
          if(image == null || image.length == 0)
              return new int[0][0];
         
-         if(image[sr][sc] == color)
-             return image;
-         
-         Queue<Integer> row = new LinkedList<Integer>();
-         Queue<Integer> col = new LinkedList();
-         int m = image.length;
-         int n = image[0].length;
-         
-         int dirs[][] = {{-1,0},{0,-1},{1,0},{0,1}};
-         int original_color = image[sr][sc];
-         row.add(sr);
-         col.add(sc);
-         image[sr][sc] = color;
-         while(!row.isEmpty())
-         {
-             int r = row.poll();
-             int c = col.poll();
-             for(int dir[] : dirs)
-             {
-                 int curr_row = dir[0] + r;
-                 int curr_col = dir[1] + c;
-                 if(curr_row>=0 && curr_row<m && curr_col>=0 && curr_col<n && image[curr_row][curr_col] == original_color)
-                 {
-                     row.add(curr_row);
-                     col.add(curr_col);
-                     image[curr_row][curr_col] = color;
-                 } 
-             }
-         }
+         if(image[sr][sc] != color)
+            dfs(image, sr, sc , color, image[sr][sc]);
         
          return image;
         
-         
-         
+    }
+    
+    public void dfs(int image[][], int sr,int sc, int color, int original_color)
+    {
+        if(sr<0 || sr>=image.length || sc<0 || sc>=image[0].length || image[sr][sc] != original_color)
+            return;
+         image[sr][sc] = color;
+         dfs(image, sr-1, sc, color, original_color);
+         dfs(image, sr+1, sc, color, original_color);
+         dfs(image, sr, sc+1, color, original_color);
+         dfs(image, sr, sc-1, color, original_color);
         
     }
 }
