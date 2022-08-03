5d51987821cfc4575f796905c93cd494985a1786
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-08-03 02:18:46 -0700

    Time: 14 ms (24.21%), Space: 47.4 MB (42.39%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,39 +10,16 @@
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         
-        Deque<TreeNode> q1 = new LinkedList();
-        Deque<TreeNode> q2 = new LinkedList();
-
-        findEle(q1, root, p);
-        findEle(q2, root, q);
-     
-        TreeNode t = null;
+        if(root == null || root == p || root == q)
+            return root;
         
-        while(!q1.isEmpty() || !q2.isEmpty())
-        {
-            if(q1.peekFirst() == q2.peekFirst())
-            { t = q1.pollFirst();
-              q2.pollFirst();
-            }
-            else
-                break;
-        }
-        return t;
-    }
-    
-    public boolean findEle(Deque<TreeNode> q, TreeNode root, TreeNode find)
-    {
-        if(root == null)
-            return false;
-         q.addLast(root);
-        if(root==find)
-            return true;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-       
-        if(findEle(q,root.left,find) || findEle(q,root.right, find))
-            return true;
-        
-        q.removeLast();
-            return false;
+        if(left != null && right != null)
+            return root;
+        if(left == null)
+            return right;
+        return left;
     }
 }
