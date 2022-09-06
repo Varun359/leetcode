f65f8babe92cc02ba1b754906ae6ed47672d36fd
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-09-05 20:45:58 -0700

    Time: 20 ms (5.39%), Space: 68.3 MB (68.60%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,42 +1,22 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
-        int result = 0, i =0,j=0;
-        int KUsed = 0;
-        int oneCounts = 0;
-        while(i<nums.length)
+        Queue<Integer> q = new LinkedList();
+        int i = 0,j =0;
+        int result = 0;
+        while(i < nums.length)
         {
-            if(nums[i] == 1)
-            {  oneCounts++;
-               i++;
-            }
-            else
+            if(nums[i] == 0)
+                q.offer(i);
+            if(q.size() == k+1)
             {
-                if(KUsed < k)
-                {
-                    KUsed++;
-                    oneCounts++;
-                    i++;
-                }
-                else if(k > 0)
-                {
-                    while(j<i && KUsed == k)
-                    {
-                        if(nums[j] == 0)
-                           KUsed--;
-                        j++;
-                        oneCounts--;
-                    }
-                }
-                else
-                {   i++;
-                    oneCounts = 0;
-                }
+                j = q.poll()+1;
             }
-            result = Math.max(oneCounts, result);
-       
-        }
         
+            result = Math.max(result, i-j+1);
+            i++;
+           
+        }
         return result;
     }
 }
