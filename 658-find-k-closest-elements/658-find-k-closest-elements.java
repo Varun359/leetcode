a8c7dd50364501e43b30ec81d10b02d2d635989a
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-09-28 22:39:41 -0700

    Time: 83 ms (12.72%), Space: 68.6 MB (8.77%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -8,18 +8,25 @@ class Solution {
             this.value = b;
         }
     }
-    public class CustomComparator implements Comparator<Pair>{
+    // public class CustomComparator implements Comparator<Pair>{
+    //     public int compare(Pair a, Pair b)
+    //     {
+    //         if(a.key == b.key)
+    //             return b.value - a.value;
+    //         return b.key - a.key;
+    //     }
+    // }
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+        PriorityQueue<Pair> pq = 
+                              new PriorityQueue<>(new Comparator<Pair>(){
         public int compare(Pair a, Pair b)
         {
             if(a.key == b.key)
                 return b.value - a.value;
             return b.key - a.key;
         }
-    }
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        
-        PriorityQueue<Pair> pq = 
-                              new PriorityQueue<>(new CustomComparator());
+                              });
         for(int i = 0;i<k;i++)
         {
             int diff = Math.abs(arr[i] - x);
