247caac8096ae1a2f8fc52206cc47baa63a6ace0
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-10-07 16:30:52 -0700

    Time: 4 ms (5.18%), Space: 42.5 MB (5.24%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,34 @@
+class Solution {
+    int dp[][];
+    public int allPossiblePaths(int i,int j, int m,int n)
+    {
+        if(i == m-1 && j == n-1)
+            return 1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        return dp[i][j] = allPossiblePaths(i+1,j,m,n) + allPossiblePaths(i,j+1,m,n);
+    }
+    public int uniquePaths(int m, int n) {
+        dp = new int[102][102];
+        for(int i =0;i<m;i++)
+        for(int j =0;j<n;j++)
+         dp[i][j] = -1;
+        return allPossiblePaths(0,0,m,n);
+    }
+}
+/*
+public class Solution {
+public int uniquePaths(int m, int n) {
+    int[][] grid = new int[m][n];
+    for(int i = 0; i<m; i++){
+        for(int j = 0; j<n; j++){
+            if(i==0||j==0)
+                grid[i][j] = 1;
+            else
+                grid[i][j] = grid[i][j-1] + grid[i-1][j];
+        }
+    }
+    return grid[m-1][n-1];
+}
+}*/
