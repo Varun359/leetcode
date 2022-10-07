3eced94ca21b3051d6dc31c18772303ce054a728
Y Varun Reddy <55958864+Varun359@users.noreply.github.com>
2022-10-07 15:33:37 -0700

    Time: 5 ms (71.40%), Space: 45.5 MB (78.34%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int char1[] = new int[26];
-        int char2[] = new int[26];
-        
-        for(int i = 0;i<ransomNote.length();i++)
-        {
-            char c = ransomNote.charAt(i);
-            char1[c - 97]++;
-        }
-        
+        int char_arr[] = new int[26];
+      
         for(int i = 0;i<magazine.length();i++)
         {
             char c = magazine.charAt(i);
-            char2[c - 97]++;
+            char_arr[c - 97]++;
         }
         
-        for(int i = 0;i<26;i++)
+        for(int i = 0;i<ransomNote.length();i++)
         {
-            if(char1[i] > char2[i])
-                return false;
+           char c = ransomNote.charAt(i);
+           if(char_arr[c - 'a'] == 0)
+               return false;
+           else
+            char_arr[c - 97]--;
         }
          return true;
     }
